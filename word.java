9293e796737dd36ffc7af9dfb76d2c08d89de25f
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-11 11:23:52 +0530

    Add files via upload

---
+++ word.java
@@ -0,0 +1,7 @@
+//Write a Java Program to Print the given word.
+public class word {
+    public static void main(String[] args) {
+        String word = "snegan";
+        System.out.println(word);
+    }
+}
