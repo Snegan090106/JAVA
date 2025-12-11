d41be6f48da5ccbb173a427b9e8f8f7fb0ff02eb
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-11 12:23:39 +0530

    Update Num_space.java

--- Num_space.java
+++ Num_space.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main Nums_pace {
+class Main Num_space {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
@@ -8,4 +8,5 @@ class Main Nums_pace {
     sc.close();
     }
 }
+
 
