90c6cdd318349d52611613f3b7ee8d6cfefcc890
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-11 14:11:50 +0530

    Update and rename num_space.java to Num_tab.java

--- Num_tab.java
+++ Num_tab.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main {
+class Num_tab {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
@@ -8,3 +8,4 @@ class Main {
     sc.close();
     }
 }
+
