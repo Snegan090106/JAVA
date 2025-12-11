0606809befae5030100b80d6fcf86e8e3e78648e
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-11 12:15:20 +0530

    Update and rename integer.java to Integer.java

--- Integer.java
+++ Integer.java
@@ -1,10 +1,11 @@
 //to print given integer number
 import java.util.Scanner;
-public class integer {
+public class Integer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         System.out.println("The given integer is: " + number);
         sc.close();
     }
+
 }
