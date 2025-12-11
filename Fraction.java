bd671161e28446fdb9a61c4757ab247fa34762db
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-11 12:13:50 +0530

    Update and rename fraction.java to Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -1,10 +1,11 @@
 //to print the given fractional number.
 import java.util.Scanner;
-public class fraction {
+public class Fraction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float number = sc.nextFloat();
         System.out.println("The given fractional number is: " + number);
         sc.close();
     }
+
 }
