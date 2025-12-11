d97f8962feea658c96258df21c23d83d769a275d
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-11 12:29:44 +0530

    Update and rename word.java to Word.java

--- Word.java
+++ Word.java
@@ -1,7 +1,8 @@
 //Write a Java Program to Print the given word.
-public class word {
+public class Word {
     public static void main(String[] args) {
         String word = "snegan";
         System.out.println(word);
     }
+
 }
