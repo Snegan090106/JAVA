5763509039483d6cc121b568dc7d550da25c601e
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-15 10:27:27 +0530

    Delete Word.java

--- Word.java
+++
@@ -1,8 +0,0 @@
-//Write a Java Program to Print the given word.
-public class Word {
-    public static void main(String[] args) {
-        String word = "snegan";
-        System.out.println(word);
-    }
-
-}
