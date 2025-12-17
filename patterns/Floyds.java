d31b87ac0e6d3bc34bac043abcd93bb43c3085de
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-18 00:05:03 +0530

    Add files via upload

---
+++ patterns/Floyds.java
@@ -0,0 +1,17 @@
+//Q22.Write a Java program to print Floyd’s Triangle.
+import java.util.Scanner;
+public class Floyds {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ patterns/Zigzag.java
@@ -0,0 +1,19 @@
+//Q21.Write a Java program to print a Zig-Zag pattern.
+import java.util.Scanner;
+public class Zigzag {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=n;j++){
+                if(((i+j)%4==0) || (i==2 && j%4==0)){
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
