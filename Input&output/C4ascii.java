4ac7780843883d28db350930fe3eafab2ddea830
Snegan090106 <23becse210@kahedu.edu.in>
2025-12-18 09:13:01 +0530

    Add files via upload

---
+++ Input&output/C4ascii.java
@@ -0,0 +1,10 @@
+//to print charecter for ascii value
+import java.util.*;
+public class C4ascii {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((char)n);
+        sc.close();
+    }
+}
